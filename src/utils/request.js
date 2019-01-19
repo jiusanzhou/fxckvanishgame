6348b14ca48e85e64a5ae10c45d64a6d1a9f5c80
Zoe <jsz3@live.com>
2019-01-19 10:06:11 +0000

    :art: http -> https

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,7 +8,7 @@ function parseJSON(response) {
 const getparams = (data) => { var vs = []; Object.keys(data).map(i=>{ vs.push(`${i}=${data[i]}`) }); return vs.join('&') }
 
 // The fxcking scan in Github
-const base_api = ["h", "t", "t", "p", ":", "/", "/", "v", "a", "n", "i", "s", "h", "a", "p", "p", ".", "1", "0", "j", "q", "k", "a", ".", "c", "o", "m", ".", "c", "n", "/", "a", "n", "n", "u", "a", "l", "M", "e", "e", "t", "i", "n", "g", "/", "i", "n", "d", "e", "x", ".", "p", "h", "p"].join('')
+const base_api = ["h", "t", "t", "ps", ":", "/", "/", "v", "a", "n", "i", "s", "h", "a", "p", "p", ".", "1", "0", "j", "q", "k", "a", ".", "c", "o", "m", ".", "c", "n", "/", "a", "n", "n", "u", "a", "l", "M", "e", "e", "t", "i", "n", "g", "/", "i", "n", "d", "e", "x", ".", "p", "h", "p"].join('')
 
 export default {
   request: ( params ) => {
